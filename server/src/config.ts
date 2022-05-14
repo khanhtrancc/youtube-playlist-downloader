61ce5fe3d55efaa5141c41902f362eb2e7fe8fa7
Khanh Tran <khanhtran.cse@gmail.com>
2022-05-14 16:28:14 +0700

    add port

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,7 @@
 
 export const config =  {
   googleApiAccessToken: process.env.YOUTUBE_ACCESS_TOKEN, //access token to list videos of playlist
-  port: 8080,
+  port: process.env.PORT || 8080,
   maxRetryCount: 5,
   retryDelayTime: 1000,
   // baseOutputPath,
